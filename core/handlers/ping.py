466dab1038f482a2035cdf9b6a05b188fbad5ebe
SkrudjReal <batyabosszrostik@gmail.com>
2025-08-23 18:47:14 +0200

    better ping

--- core/handlers/ping.py
+++ core/handlers/ping.py
@@ -6,13 +6,16 @@ from asyncmy.pool import Pool
 
 from core.data.tricks import tricks
 from core.utils.db_api.repo import RequestsRepo
+from core import func
+
 import time
 
 async def ping(app: Client, msg: Message, me: User, pool: Pool, repo: RequestsRepo, redis: Redis):
 
-    time_start = time.time()
-    mesg = await msg.reply(tricks['ping']['ping'], quote=False)
-    time_end = time.time()
-    await mesg.edit_text(tricks['ping']['ping_speed'].format(f'{time_end-time_start:.3f}'))
-    await msg.delete()
+    result_ping = ''
 
+    time_start = time.perf_counter_ns()
+    mesg = await msg.reply(tricks['ping']['ping'], quote=False)
+    result_ping += str(round((time.perf_counter_ns() - time_start) / 10**6, 3))
+    await mesg.edit_text(tricks['ping']['ping_speed'].format(result_ping))
+    await msg.delete()
